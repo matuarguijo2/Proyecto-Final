029303cccf9e898f4524a3ab1e79b2dead13bb0a
matuarguijo2 <matuarguijo2@gmail.com>
2025-09-05 18:01:17 -0300

    proyectofinal

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+require('dotenv').config();
+import express from "express";
+import userRoutes from "./routes/userRoutes";
+
+
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json())
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Servidor corriendo con ts-node 🚀" });
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Servidor escuchando en http://localhost:${PORT}`);
+});
---
+++ backend/src/routes/userRoutes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const router = Router();
+
+// Crear usuario
+router.post("/", async (req, res) => {
+  try {
+    const { dni, nombre, apellido, email, password, rol } = req.body;
+    const user = await prisma.user.create({
+      data: { dni, nombre, apellido, email, password, rol },
+    });
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al crear usuario" });
+  }
+});
+
+// Listar usuarios
+router.get("/", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+export default router;
