775cfb5cf621993c5e3f74fef0dff9cdf0f31402
matuarguijo2 <matuarguijo2@gmail.com>
2025-09-12 16:28:10 -0300

    agregado de jwt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 require('dotenv').config();
 import express from "express";
 import userRoutes from "./routes/userRoutes";
+import dotenv from "dotenv";
+dotenv.config();
 
 
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json())
 app.use("/api", userRoutes);
